6cdaa29fe6026b9c1d369d4a502f5089bc3d2147
Inga <engaezik@gmail.com>
2023-02-24 00:08:41 +0300

    Entering a new number

--- ClassWork3/src/FoolCheck.java
+++ ClassWork3/src/FoolCheck.java
@@ -1,15 +1,25 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FoolCheck {
+
+    public static void methodToTest(int number){
+        int res = 100 / number;
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String word;
+//        String word;
+//
+//        do{
+//            word = scan.next();
+//        }while(word.length() > 10);
 
-        do{
-            word = scan.next();
-        }while(word.length() > 10);
+        try{
+            int a = scan.nextInt();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Введено не число");
+        }
 
-//        System.out.println("Слово слишком длинное");
-//        word = scan.next();
     }
 }
