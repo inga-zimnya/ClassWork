c4b06df4fb2f74332d832b516bcc911517b00833
Inga <engaezik@gmail.com>
2023-03-09 19:42:17 +0300

    Project of a school with amount of students and the current season

--- ClassWork5_Gen/src/ru/school/School.java
+++ ClassWork5_Gen/src/ru/school/School.java
@@ -1,4 +1,32 @@
 package ru.school;
 
 public class School {
+    int amount;
+    Season season;
+
+    public enum Season{
+        WINTER("Зима", "Winter"),
+        SPRING("Весна", "Spring"),
+        SUMMER("Лето", "Summer"),
+        AUTUMN("Осень", "Autumn");
+
+        private String name;
+        private String engName;
+
+
+        Season(String name, String engName){
+            this.name = name;
+            this.engName = engName;
+        }
+
+        @Override
+        public String toString(){
+            return name;
+        }
+    }
+
+    public School(int amount, Season season){
+        this.amount = amount;
+        this.season = season;
+    }
 }
