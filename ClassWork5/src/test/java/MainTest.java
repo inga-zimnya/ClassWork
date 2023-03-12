09333cf08e0f7d42915975b404dd6627c5ca9f6a
Inga <engaezik@gmail.com>
2023-03-12 14:20:27 +0300

    Parametrized test with library of Arguments

--- ClassWork5/src/test/java/MainTest.java
+++ ClassWork5/src/test/java/MainTest.java
@@ -2,6 +2,11 @@ import org.example.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest {
     Main main;
@@ -33,4 +38,24 @@ public class MainTest {
         //not equal, потому что это 2 разные ссылки на разные объекты
 
     }
+
+    @ParameterizedTest
+    @MethodSource("numbers")
+    public void shouldReturnStringWithNumberWithTwoNumbers(int number1, int number2, String answer){
+        String res = main.returnStringWithNumber(number1, number2);
+
+        Assertions.assertEquals(answer, res);
+    }
+
+    private static Stream<Arguments> numbers(){
+        return Stream.of(
+                Arguments.of(1, 2, "number 12"),
+                Arguments.of(-1, -2, "number -1-2"),
+                Arguments.of(22, 44, "number 2244"),
+                Arguments.of(84756745, 27658756, "number 8475674527658756")
+        );
+    }
+
+
+
 }
